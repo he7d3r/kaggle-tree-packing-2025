7f5d87b20065699de0d2325d3ae2f76c9c42a071
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2025-11-24 20:17:08 -0300

    chore: move code from notebook to main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+from solver.solver import solve_all
+from submission.submission import make_submission_df
+
+
+def main() -> None:
+    rng = random.Random(42)
+    tree_data = solve_all(rng)
+    output_file = "sample_submission.csv"
+    df = make_submission_df(tree_data)
+    df.to_csv(output_file)
+    print(f"Submission saved to {output_file}")
+
+
+if __name__ == "__main__":
+    main()
