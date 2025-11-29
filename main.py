c2f8fcece8517fa8b9f57293950b7c334ad17fd8
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2025-11-29 08:43:30 -0300

    feat: only log a run on mlflow if the flag is provided

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import random
 
 import mlflow
@@ -9,31 +10,54 @@ from solver import solve_all
 from submission import make_submission_df
 
 OUTPUT_FILE = "submission.csv"
+TRACKING_URI = "sqlite:///mlruns.db"
+EXPERIMENT_NAME = "Christmas Tree Packing"
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--mlflow", action="store_true", help="Enable MLflow logging"
+    )
+    return parser.parse_args()
 
 
 def main() -> None:
-    mlflow.set_tracking_uri("sqlite:///mlruns.db")
-    mlflow.set_experiment("Christmas Tree Packing")
-    with mlflow.start_run():
-        try:
-            rng = random.Random(42)
-            plotter = Plotter()
-            tree_data = solve_all(rng, plotter)
-            df = make_submission_df(tree_data)
-            df.to_csv(OUTPUT_FILE)
-            print(f"Submission saved to {OUTPUT_FILE}")
+    args = parse_args()
 
-            df = pd.read_csv(
-                OUTPUT_FILE,
-                dtype={"x": "string", "y": "string", "deg": "string"},
-            )
-            submission_score = score(df)
+    if args.mlflow:
+        mlflow.set_tracking_uri(TRACKING_URI)
+        mlflow.set_experiment(EXPERIMENT_NAME)
+        run = mlflow.start_run()
+    else:
+        run = None  # no MLflow
+
+    try:
+        rng = random.Random(42)
+        plotter = Plotter()
+        tree_data = solve_all(rng, plotter)
+        df = make_submission_df(tree_data)
+        df.to_csv(OUTPUT_FILE)
+        print(f"Submission saved to {OUTPUT_FILE}")
+
+        df = pd.read_csv(
+            OUTPUT_FILE,
+            dtype={"x": "string", "y": "string", "deg": "string"},
+        )
+        submission_score = score(df)
+        print(f"Submission score: {submission_score}")
+
+        if args.mlflow:
             mlflow.log_metric("submission_score", submission_score)
-            print(f"Submission score: {submission_score}")
-        except Exception as e:
+
+    except Exception as e:
+        if args.mlflow:
             mlflow.log_param("error", str(e))
             mlflow.end_run(status="FAILED")
-            raise
+        raise
+    finally:
+        if args.mlflow and run is not None:
+            mlflow.end_run()
 
 
 if __name__ == "__main__":
