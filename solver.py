5cf98718f56665b744aba22a64152e3463419929
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2025-12-07 16:27:13 -0300

    feat: Fix collision detection and allow 5 by 5 degree rotations

--- solver.py
+++ solver.py
@@ -11,11 +11,11 @@ from solution import Solution
 
 
 def get_default_solver() -> "Solver":
-    return Solver(name="Tighter Grid With Smaller Rotation")
+    return Solver(name="Tighter grid with 5 by 5 degree rotations")
 
 
 class Solver:
-    ANGLES: tuple[int, ...] = (0, 30, 60, 90)
+    ANGLES: tuple[int, ...] = tuple(range(0, 90 + 5, 5))
     WIDTH_INCREMENTS: tuple[int, ...] = (-1, 0)
 
     def __init__(self, name: str):
@@ -61,21 +61,34 @@ class Solver:
         dy = height
         polygon = tree.polygon
         while dx > 0:
-            moved = affinity.translate(
+            moved_x = affinity.translate(
                 polygon, xoff=float(to_scale(dx)), yoff=0.0
             )
-            if self._relevant_collision(moved, polygon):
+            if self._relevant_collision(moved_x, polygon):
                 break
             dx -= dx_step_in
-        # FIXME: The tree at (0, 1) could collide with (1, 0) but not (0, 0)
+        dx += dx_step_in
         while dy > 0:
-            moved = affinity.translate(
+            moved_y = affinity.translate(
                 polygon, xoff=0.0, yoff=float(to_scale(dy))
             )
-            if self._relevant_collision(moved, polygon):
+            if self._relevant_collision(moved_y, polygon):
+                break
+            # The trees at (1, 0) and (0, 1) might collide
+            moved_x = affinity.translate(
+                polygon, xoff=float(to_scale(dx)), yoff=0.0
+            )
+            if self._relevant_collision(moved_x, moved_y):
+                break
+            # The trees at (1, 1) and (0, 0) might collide
+            moved_xy = affinity.translate(
+                polygon, xoff=float(to_scale(dx)), yoff=float(to_scale(dy))
+            )
+            if self._relevant_collision(moved_xy, polygon):
                 break
             dy -= dy_step_in
-        return dx + dx_step_in, dy + dy_step_in
+        dy += dy_step_in
+        return dx, dy
 
     def _relevant_collision(self, a: Polygon, b: Polygon) -> bool:
         return a.intersects(b) and not a.touches(b)
